8eddc7939a374a5e0d2a7fc4101c1329dc2ca5db
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2024-03-17 00:35:15 +0700

    removed unnecessary file(q.txt)

--- __tests__/aiAnswer.test.js
+++ __tests__/aiAnswer.test.js
@@ -3,13 +3,13 @@ const app = require("../utils/createServer");
 const ai = require("../ai/ai");
 
 jest.mock("../ai/ai", () => ({
-  run: jest.fn(() => "some answer text"),
+  run: jest.fn(),
 }));
-
 describe("aiAnswers", () => {
   describe("Generate aiAnswers", () => {
     describe("Given input(question) provided and suppose run() runs correctly", () => {
       it("should response a 200 and an object with answer property", async () => {
+        ai.run.mockReturnValue("some answer text");
         const { statusCode, body } = await request(app)
           .post("/api/v1/aiAnswers")
           .send({ question: "some question text" });
@@ -21,10 +21,21 @@ describe("aiAnswers", () => {
     });
 
     describe("Given input(question) not provided or input(question) is provided but empty string", () => {
-      it("should response a 400 and a text saying 'Question must be provided.'", async () => {
+      it("should response a 400 and an object with message saying 'Question must be provided.'", async () => {
         const res = await request(app).post("/api/v1/aiAnswers");
         expect(res.statusCode).toBe(400);
         expect(res.body).toEqual({ message: "Question must be provided." });
+      });
+    });
+
+    describe("Given run() throws", () => {
+      it("should response a 500", async () => {
+        ai.run.mockRejectedValue(new Error("mock error"));
+        const res = await request(app)
+          .post("/api/v1/aiAnswers")
+          .send({ question: "some question text" });
+        expect(res.statusCode).toBe(500);
+        expect(ai.run).toHaveBeenCalledWith("some question text");
       });
     });
   });
