bbded75251e7a85c311e34991f5b8938b597b8b0
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2024-03-17 16:54:20 +0700

    fixed cors middleware placement

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 require("dotenv").config();
 const app = require("./utils/createServer");
 
-//imports
-const cors = require("cors");
-const morgan = require("morgan");
-
-//middleware
-const allowedOrigins = ["https://maypar.netlify.app", "http://localhost:5173"];
-app.use(
-  cors({
-    origin: allowedOrigins,
-  })
-);
-app.use(morgan("tiny"));
-
 //start the server
 const port = process.env.PORT || 5000;
 const start = async () => {
--- utils/createServer.js
+++ utils/createServer.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const app = express();
-
+const cors = require("cors");
+const morgan = require("morgan");
 const aiAnswerRouter = require("../router/aiAnswerRouter");
 
+const app = express();
+
+//middleware
+const allowedOrigins = ["https://maypar.netlify.app", "http://localhost:5173"];
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use("/api/v1/aiAnswers", aiAnswerRouter);
